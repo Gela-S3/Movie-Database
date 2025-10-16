88382740ef06eb8183bd955598df9525b3e34395
Gelagay G. <gela.sirta3@gmail.com>
2025-10-16 04:18:26 +0300

    Add pagination and styling to movie listings; enhance single movie view with navigation

--- Movie/src/component/Home.jsx
+++ Movie/src/component/Home.jsx
@@ -1,45 +1,52 @@
-import React, { use, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { NavLink } from 'react-router-dom';
 
-
 const Home = () => {
-
   const [movieAllData, setMovieAllData] = React.useState([]);
   const [inputValue, setInputValue] = React.useState("");
   const [searchKeyword, setSearchKeyword] = React.useState("movie");
   const [isDataAvailable, setIsDataAvailable] = React.useState(true);
+  const [page, setPage] = React.useState(1); 
+  const [totalResults, setTotalResults] = React.useState(0); 
 
   const getMoviedata = async () => {
     try {
-      const { data } = await axios.get(`http://www.omdbapi.com/?apikey=e9b7fc46&s=${searchKeyword}`);
-      if (data?.Response == "True") {
+      const { data } = await axios.get(
+        `http://www.omdbapi.com/?apikey=e9b7fc46&s=${searchKeyword}&page=${page}`
+      );
+      if (data?.Response === "True") {
         setMovieAllData(data?.Search);
-        setIsDataAvailable(true)
-      }
-      if (data?.Response == "False") {
-
+        setTotalResults(Number(data?.totalResults));
+        setIsDataAvailable(true);
+      } else {
         setIsDataAvailable(false);
       }
-
     } catch (error) {
       console.log("error:", error);
     }
+  };
 
-  }
   useEffect(() => {
     getMoviedata();
-  }, [searchKeyword]);
-
+  }, [searchKeyword, page]); 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
   const handleSearch = () => {
     setSearchKeyword(inputValue);
-  }
+    setPage(1);
+  };
 
 
+  const handleNext = () => {
+    if (page * 10 < totalResults) setPage(page + 1);
+  };
+
+  const handlePrev = () => {
+    if (page > 1) setPage(page - 1);
+  };
 
   return (
     <>
@@ -47,44 +54,89 @@ const Home = () => {
         <div className="row px-4 ">
           <div className="col-6 d-flex flex-column gap-5">
             <div>
-              <h1 className='heading-color'>WatchY our Favourite Here!</h1>
-              <p className='heading-color'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa veritatis rerum similique praesentium explicabo perferendis error, numquam, dignissimos perspiciatis fugit quis omnis? Iure beatae necessitatibus inventore sint quia deserunt error?
+              <h1 className="heading-color">Watch Your Favourite Here!</h1>
+              <p className="heading-color">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+                veritatis rerum similique praesentium explicabo perferendis
+                error, numquam, dignissimos perspiciatis fugit quis omnis? Iure
+                beatae necessitatibus inventore sint quia deserunt error?
               </p>
             </div>
-            <div className='d-flex gap-2 '>
-              <input type="text" className='input-field w-75  border-0 px-2' placeholder='Search for a movie'
+            <div className="d-flex gap-2 ">
+              <input
+                type="text"
+                className="input-field w-75 border-0 px-2"
+                placeholder="Search for a movie"
                 value={inputValue}
                 onChange={handleInputChange}
               />
-              <button className='w-25 border-0 search-button' onClick={handleSearch}>Search</button>
+              <button
+                className="w-25 border-0 search-button"
+                onClick={handleSearch}
+              >
+                Search
+              </button>
             </div>
-
           </div>
         </div>
+
         <div className="row px-4 my-5 row-gap-4 gap-3 justify-content-between ">
-          {isDataAvailable ?movieAllData?.map((movie) => {
-              return (
-                <div className="col-2" key={movie?.imdbID}>
-                  <NavLink  className={"text-decoration-none" } to={`/single/${movie?.imdbID}`}>
-                  <div className='card-bg rounded'>
+          {isDataAvailable ? (
+            movieAllData?.map((movie) => (
+              <div className="col-2" key={movie?.imdbID}>
+                <NavLink
+                  className={"text-decoration-none"}
+                  to={`/single/${movie?.imdbID}`}
+                >
+                  <div className="card-bg rounded">
                     <div>
-                      <img src={movie?.Poster} alt="movie-cart" className='movie-card p-2 pb-0' />
+                      <img
+                        src={movie?.Poster}
+                        alt="movie-cart"
+                        className="movie-card p-2 pb-0"
+                      />
                     </div>
-                    <div className='py-2'>
-                      <p className='heading-color fs-5 text-center m-0 text-overflow px-2'>{movie?.Title}</p>
+                    <div className="py-2">
+                      <p className="heading-color fs-5 text-center m-0 text-overflow px-2">
+                        {movie?.Title}
+                      </p>
                     </div>
                   </div>
-                  </NavLink>
-                </div>
-
-              )
-            }): <h1 className='text-center heading-color '>No Result Found</h1>
-          }
-
+                </NavLink>
+              </div>
+            ))
+          ) : (
+            <h1 className="text-center heading-color ">No Result Found</h1>
+          )}
         </div>
+
+        {isDataAvailable && movieAllData.length > 0 && (
+          <div className="pagination-container d-flex justify-content-center align-items-center gap-3 my-4">
+            <button
+              className="pagination-btn prev-btn"
+              onClick={handlePrev}
+              disabled={page === 1}
+            >
+              ◀ Previous
+            </button>
+
+            <span className="heading-color fw-bold">
+              Page {page} of {Math.ceil(totalResults / 10) || 1}
+            </span>
+
+            <button
+              className="pagination-btn next-btn"
+              onClick={handleNext}
+              disabled={page * 10 >= totalResults}
+            >
+              Next ▶
+            </button>
+          </div>
+
+        )}
       </div>
     </>
   );
-}
+};
 
 export default Home;
--- Movie/src/component/Single.jsx
+++ Movie/src/component/Single.jsx
@@ -1,30 +1,46 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Single = () => {
-
-  const {id} = useParams();
-  
+  const { id } = useParams();
+  const navigate = useNavigate();
   const [singleMovie, setSingleMovie] = useState("");
 
-  const getSingleData = async () =>{
-    const data  = await axios.get(`http://www.omdbapi.com/?apikey=e9b7fc46&i=${id}`);
-    console.log("single-data:", data);
-    setSingleMovie(data?.data);
-  }
-  useEffect(()=>{
+  const getSingleData = async () => {
+    try {
+      const { data } = await axios.get(
+        `http://www.omdbapi.com/?apikey=e9b7fc46&i=${id}`
+      );
+      setSingleMovie(data);
+    } catch (error) {
+      console.log("Error fetching single movie:", error);
+    }
+  };
+
+  useEffect(() => {
     getSingleData();
-  },[id])
+  }, [id]);
+
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
 
   return (
     <div className="container-fluid">
       <div className="row px-4 my-4">
+
         <div className="col-4">
           <div>
-            <img src={singleMovie?.Poster} alt={singleMovie?.Title} className="single-poster"/>
+            <img
+              src={singleMovie?.Poster}
+              alt={singleMovie?.Title}
+              className="single-poster"
+            />
           </div>
         </div>
+
         <div className="col-8">
           <div>
             <h2 className="text-light">{singleMovie?.Title}</h2>
@@ -32,29 +48,47 @@ const Single = () => {
           <div>
             <p className="text-light text-grey">{singleMovie?.Plot}</p>
           </div>
+
           <div className="mt-2">
             <p className="m-0 fs-5 text-grey">Rating</p>
-            <p className="m-0  card-bg py-1 px-2 text-yellow rounded d-inline-block"><span><i className="fa-solid fa-star font-size-12"></i></span>{singleMovie?.imdbRating}</p>
+            <p className="m-0 card-bg py-1 px-2 text-yellow rounded d-inline-block">
+              <span>
+                <i className="fa-solid fa-star font-size-12"></i>
+              </span>{" "}
+              {singleMovie?.imdbRating}
+            </p>
           </div>
+
           <div className="mt-2">
             <p className="m-0 fs-5 text-grey">Country</p>
             <p className="m-0 text-light font-size-14">
-              {singleMovie?.Country}</p>
+              {singleMovie?.Country}
+            </p>
           </div>
+
           <div className="mt-2">
             <p className="m-0 fs-5 text-grey">Language</p>
             <p className="m-0 text-light font-size-14">
-              {singleMovie?.Language}</p>
+              {singleMovie?.Language}
+            </p>
           </div>
+
           <div className="mt-2">
             <p className="m-0 fs-5 text-grey">Duration</p>
             <p className="m-0 text-light font-size-14">
-              {singleMovie?.Runtime}</p>
+              {singleMovie?.Runtime}
+            </p>
           </div>
+
           <div className="mt-2">
             <p className="m-0 fs-5 text-grey">Year</p>
-            <p className="m-0 text-light font-size-14">
-              {singleMovie?.Year}</p>
+            <p className="m-0 text-light font-size-14">{singleMovie?.Year}</p>
+          </div>
+
+          <div className="col-12 py-4">
+            <button className="btn-back search-button" onClick={handleGoBack}>
+              ← Go Back
+            </button>
           </div>
         </div>
       </div>
@@ -63,7 +97,3 @@ const Single = () => {
 };
 
 export default Single;
-
-
-
-
